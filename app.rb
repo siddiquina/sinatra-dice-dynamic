81e8a259b392e03494f7a57876ea99606f40d4dc
siddiquina <137005373+siddiquina@users.noreply.github.com>
2023-07-13 00:23:59 +0000

    Refactor!

--- app.rb
+++ app.rb
@@ -5,43 +5,20 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/2/6") do
-  first_die = rand(1..6)
-	second_die = rand(1..6)
-  sum = first_die + second_die
-	
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-	erb(:two_six)
-end
 
-get("/dice/2/10") do
-  first_die = rand(1..10)
-	second_die = rand(1..10)
-  sum = first_die + second_die
-	
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  erb(:two_ten)
-end
 
-get("/dice/1/20") do
-  @die = rand(1..20)
-	
-	@outcome = "You rolled a #{@die}."
-	
-	erb(:one_twenty)
-end
+get("/dice/:number_of_dice/:how_many_sides") do
+  @num_dice = params.fetch("number_of_dice").to_i
 
-get("/dice/5/4") do
-  first_die = rand(1..4)
-	second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
-	
-	@outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
-	
-  erb(:five_four)
+  @sides = params.fetch("how_many_sides").to_i
+
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
 end
